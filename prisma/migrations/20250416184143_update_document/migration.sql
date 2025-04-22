fc5334163f5fbd84bfc8716949ee4b66d8018d5d
Júlia <juliamenzss@gmail.com>
2025-04-22 15:20:38 -0300

    feat: adiciona projeto

--- prisma/migrations/20250416184143_update_document/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `archivematicaId` on table `documents` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "documents" ALTER COLUMN "archivematicaId" SET NOT NULL;
